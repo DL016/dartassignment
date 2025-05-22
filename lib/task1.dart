ffffa6ef921ff6729bcc5358224ba4d825f3f672
Itam_james <itamjames111@gmail.com>
2025-05-22 01:08:51 +0100

    initialcommit

---
+++ lib/task1.dart
@@ -0,0 +1,26 @@
+
+
+int account_balance({int balance = 5000}){
+  return balance;
+}
+
+
+// int deposit(balance, amount){
+//   int deposited_amount = balance + amount;
+//   return deposited_amount;
+// }
+
+
+int mine({required int amount, required int balance}){
+  int deposited_amount = balance + amount;
+
+  return deposited_amount;
+}
+
+
+int withdraw({required int amount, required int balance}){
+  int withdrawed = balance - amount;
+
+  return withdrawed;
+
+}
